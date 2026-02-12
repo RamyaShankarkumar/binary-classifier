19ca6f8dcdd65c2b2c76d4d83583a143246abb26
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2026-02-12 16:07:34 -0500

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,16 +1,18 @@
 import torch
 import torch.nn as nn
 
-class BinaryClassifier(nn.Module):
-    def __init__(self, input_dim):
-        super(BinaryClassifier, self).__init__()
+class MyModel(nn.Module):
+    def __init__(self, input_size=30):
+        super(MyModel, self).__init__()
+
+        self.input_size = input_size
 
         self.network = nn.Sequential(
-            nn.Linear(input_dim, 16),
+            nn.Linear(input_size, 16),
             nn.ReLU(),
             nn.Linear(16, 8),
             nn.ReLU(),
-            nn.Linear(8, 1)  # logits
+            nn.Linear(8, 1)
         )
 
     def forward(self, x):
