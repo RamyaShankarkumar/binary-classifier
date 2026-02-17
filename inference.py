5ce8dbecf6e9cef589bfc9ca3b4d69c00cac7cf1
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2026-02-16 21:54:03 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -10,7 +10,18 @@ model.load_state_dict(torch.load("model.pth"))
 model.eval()
 
 # Sample inference input (5 samples)
-sample_inputs = torch.randn(5, INPUT_SIZE)
+#sample_inputs = torch.randn(5, INPUT_SIZE)
+
+# Sum > 0 → class 1
+# Sum < 0 → class 0
+sample_inputs = torch.tensor([
+    [1.0]*30,   # sum = 30 → class 1
+    [-1.0]*30,  # sum = -30 → class 0
+    [0.5]*30,   # sum = 15 → class 1
+    [-0.5]*30,  # sum = -15 → class 0
+    [0.0]*30    # sum = 0 → borderline
+])
+
 
 with torch.no_grad():
     outputs = model(sample_inputs)
