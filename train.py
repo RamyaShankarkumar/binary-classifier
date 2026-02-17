f5d2a297b84a431f3b9083811beb7f33fe05e639
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2026-02-16 21:52:31 -0500

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,8 @@ LR = 0.001
 
 # Dummy dataset
 X = torch.randn(200, INPUT_SIZE)
-y = torch.randint(0, 2, (200, 1)).float()
+#y = torch.randint(0, 2, (200, 1)).float()
+y = (X.sum(dim=1) > 0).float().unsqueeze(1)
 
 model = MyModel(input_size=INPUT_SIZE)
 
